95fb099a98b8537accd496e456b022b037b00f69
wagnerlemos94 <wagner.lemos94@hotmail.com>
2021-06-30 20:12:48 -0300

    Metodo de buscar lancamentos

--- src/main/java/com/dsousa/minnhasfinancas/api/resource/LancamentoResource.java
+++ src/main/java/com/dsousa/minnhasfinancas/api/resource/LancamentoResource.java
@@ -1,13 +1,18 @@
 package com.dsousa.minnhasfinancas.api.resource;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dsousa.minnhasfinancas.api.dto.LancamentoDTO;
@@ -19,6 +24,7 @@ import com.dsousa.minnhasfinancas.model.enuns.TipoLancamento;
 import com.dsousa.minnhasfinancas.service.LancamentoService;
 import com.dsousa.minnhasfinancas.service.UsuarioService;
 
+
 @RestController
 @RequestMapping("/api/lancamentos")
 public class LancamentoResource {
@@ -28,6 +34,28 @@ public class LancamentoResource {
 
 	public LancamentoResource(LancamentoService service) {
 		this.service = service;
+	}
+
+	@GetMapping
+	public ResponseEntity buscar(@RequestParam(value = "descricao", required = false) String descricao,
+			@RequestParam(value = "mes", required = false) Integer mes,
+			@RequestParam(value = "ano", required = false) Integer ano,
+			@RequestParam(value = "usuario") Long idUsuario) {
+		Lancamento lancamentoFiltro = new Lancamento();
+		lancamentoFiltro.setDescricao(descricao);
+		lancamentoFiltro.setMes(mes);
+		lancamentoFiltro.setAno(ano);
+
+		Optional<Usuario> usuario = usuarioService.obterPorId(idUsuario);
+		if (usuario.isPresent()) {
+			return ResponseEntity.badRequest()
+					.body("Não foi possísel realizar a consulta. Usuário não encontrado para o Id informado");
+		}else {
+			lancamentoFiltro.setUsuario(usuario.get());
+		}
+		
+		List<Lancamento> lancamentos = service.buscar(lancamentoFiltro);
+		return ResponseEntity.ok(lancamentos);
 	}
 
 	@PostMapping
@@ -51,18 +79,16 @@ public class LancamentoResource {
 				return ResponseEntity.ok(lancamento);
 			} catch (RegraNegocioException e) {
 				return ResponseEntity.badRequest().body(e.getMessage());
-			}			
-		}).orElseGet(() -> 
-		new  ResponseEntity("Lancamento não encontrado na base de Dados.", HttpStatus.BAD_REQUEST));
+			}
+		}).orElseGet(() -> new ResponseEntity("Lancamento não encontrado na base de Dados.", HttpStatus.BAD_REQUEST));
 	}
-	
+
 	@DeleteMapping("{id}")
 	public ResponseEntity delete(@PathVariable("id") Long id) {
 		return service.obterPorId(id).map(entidade -> {
 			service.deletar(entidade);
 			return new ResponseEntity(HttpStatus.NO_CONTENT);
-		}).orElseGet(() -> 
-		new  ResponseEntity("Lancamento não encontrado na base de Dados.", HttpStatus.BAD_REQUEST));
+		}).orElseGet(() -> new ResponseEntity("Lancamento não encontrado na base de Dados.", HttpStatus.BAD_REQUEST));
 	}
 
 	private Lancamento converter(LancamentoDTO dto) {
