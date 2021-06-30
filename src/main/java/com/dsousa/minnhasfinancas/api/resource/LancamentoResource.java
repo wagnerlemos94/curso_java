d15b6fcc8722a863f6730820d9e8a76f7c318ccc
wagnerlemos94 <wagner.lemos94@hotmail.com>
2021-06-30 19:55:40 -0300

    Metodo de deletar lancamento

--- src/main/java/com/dsousa/minnhasfinancas/api/resource/LancamentoResource.java
+++ src/main/java/com/dsousa/minnhasfinancas/api/resource/LancamentoResource.java
@@ -2,6 +2,7 @@ package com.dsousa.minnhasfinancas.api.resource;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -51,6 +52,15 @@ public class LancamentoResource {
 			} catch (RegraNegocioException e) {
 				return ResponseEntity.badRequest().body(e.getMessage());
 			}			
+		}).orElseGet(() -> 
+		new  ResponseEntity("Lancamento não encontrado na base de Dados.", HttpStatus.BAD_REQUEST));
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity delete(@PathVariable("id") Long id) {
+		return service.obterPorId(id).map(entidade -> {
+			service.deletar(entidade);
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
 		}).orElseGet(() -> 
 		new  ResponseEntity("Lancamento não encontrado na base de Dados.", HttpStatus.BAD_REQUEST));
 	}
