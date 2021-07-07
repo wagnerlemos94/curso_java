fb2ba356dc51a831878d583cbec8a6e57307f581
wagnerlemos94 <wagner.lemos94@hotmail.com>
2021-07-06 23:07:45 -0300

    configuracao de cors

--- src/main/java/com/dsousa/minnhasfinancas/api/dto/UsuarioDTO.java
+++ src/main/java/com/dsousa/minnhasfinancas/api/dto/UsuarioDTO.java
@@ -1,11 +1,15 @@
 package com.dsousa.minnhasfinancas.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Builder
 @Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioDTO {
 
 	private String email;
